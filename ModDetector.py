f2821b3a0512466993d843b6c0130a3777bdb456
bcoltonknight <40767566+bcoltonknight@users.noreply.github.com>
2021-09-06 20:09:36 -0400

    Update ModDetector.py
    Added comments to improve clarity, as well as a live updating text to show the scan progress. Also added a splash screen.

--- ModDetector.py
+++ ModDetector.py
@@ -3,38 +3,63 @@ import ipaddress
 from pymodbus.exceptions import *
 import sys
 
-Endpoints = []
-IPs = []
-Subnet = ''
 
-if len(sys.argv) == 2:
-    if sys.argv[1] == '-h' or sys.argv[1] == '--help':
-        print('''Usage: python3 ModDetector.py [-h] [Subnet]''')
-        quit()
-    Subnet = sys.argv[1]
-else:
-    print('Invalid Arguments. Format python3 ModDetector.py [Subnet]')
+if __name__ == '__main__':
+    Endpoints = []
+    IPs = []
+    Subnet = ''
 
-try:
-    IPs = [str(ip) for ip in ipaddress.IPv4Network(Subnet)]
-except ipaddress.AddressValueError:
-    print('Invalid Subnet')
+    print('''-------------------------------------------------------------------------------
+ #     #               ######                                                 
+ ##   ##  ####  #####  #     # ###### ##### ######  ####  #####  ####  #####  
+ # # # # #    # #    # #     # #        #   #      #    #   #   #    # #    # 
+ #  #  # #    # #    # #     # #####    #   #####  #        #   #    # #    # 
+ #     # #    # #    # #     # #        #   #      #        #   #    # #####  
+ #     # #    # #    # #     # #        #   #      #    #   #   #    # #   #  
+ #     #  ####  #####  ######  ######   #   ######  ####    #    ####  #    # 
+-------------------------------------------------------------------------------''')
 
-print('Beginning Scan...')
+    # Check valid number of arguments
+    if len(sys.argv) == 2:
+        # Print help text
+        if sys.argv[1] == '-h' or sys.argv[1] == '--help':
+            print('''Usage: python3 ModDetector.py [-h] [Subnet]''')
+            quit()
 
-for ip in IPs:
+        # Load Subnet as a variable
+        Subnet = sys.argv[1]
+    else:
+        print('Invalid Arguments. Format python3 ModDetector.py [Subnet]')
+
+    # Try to generate ip list from subnet
     try:
-        client = ModbusTcpClient(ip)
-        print(f'{ip}: {client.read_holding_registers(11)}')
-        Endpoints.append(ip)
-
-    except ConnectionException:
-        continue
-
-    except KeyboardInterrupt:
+        IPs = [str(ip) for ip in ipaddress.IPv4Network(Subnet)]
+    except ipaddress.AddressValueError:
+        print('Invalid Subnet')
         quit()
 
-print(f'Scan Complete. {len(Endpoints)} Found')
+    except:
+        print('Error resolving Subnet')
+        quit()
 
-for i in Endpoints:
-    print(i)
+    print('Beginning Scan...')
+
+    # Iterate over IPs in the subnet, and print ones with valid responses
+    for ip in IPs:
+        print(f'Testing... {ip}', end="\r")
+        try:
+            client = ModbusTcpClient(ip)
+            print(f'{ip}: {client.read_holding_registers(11)}')
+            Endpoints.append(ip)
+
+        except ConnectionException:
+            continue
+
+        except KeyboardInterrupt:
+            break
+
+    # Print Valid endpoints found
+    print(f'\n\nScan Complete. {len(Endpoints)} Found')
+
+    for i in Endpoints:
+        print(i)
